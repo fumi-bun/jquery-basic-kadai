cf162a6c09f648f92859a6a9e74b4dcdbbafc1e9
fumi-bun <150202074+fumi-bun@users.noreply.github.com>
2023-12-18 21:29:14 +0900

    Delete kadai_004/event.js

--- kadai_004/event.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', () => {
-   //HTMLドキュメントが読み込み完了したとき 
-   console.log('loadイベントが発生しました');
-  });
-$(window).on('scroll', () => {
-  //画面をスクロールしたとき 
-  console.log('scrollイベントが発生しました');
-  });
